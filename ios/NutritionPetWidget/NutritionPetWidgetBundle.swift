4606eace0956a59dd7c3659ea081b6acdae84500
Irwan-Shah <irwanshah0990@gmail.com>
2026-04-05 17:36:56 +0800

    widget for pet on home screen

---
+++ ios/NutritionPetWidget/NutritionPetWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  NutritionPetWidgetBundle.swift
+//  NutritionPetWidget
+//
+//  Created by Irwan Shah on 04/04/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NutritionPetWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NutritionPetWidget()
+        NutritionPetWidgetControl()
+        NutritionPetWidgetLiveActivity()
+    }
+}
